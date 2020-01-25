52c271b1dacf2ddce57624de0696a78d994779e6
Supe, Neha <nehasupe@iu.edu>
2020-01-24 19:55:58 -0500

    Update xsh_hello.c

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -9,17 +9,17 @@ shellcmd xsh_hello(int nargs, char *args[])
 		/* nargs is number of arguments passed through commandline 
 		 * 	and args is the array of arrguments passed*/
 		if (nargs == 2) {
-					printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
-						}
+			printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
+		}
 		else {
-					if (nargs > 2){
-						fprintf(stderr, "Too many arguments\n");
-					}
-					if (nargs < 2){
-						fprintf(stderr, "Too few arguments\n");
-						}
-					return SYSERR;
-											}
+			if (nargs > 2){
+				fprintf(stderr, "Too many arguments\n");
+			}
+			if (nargs < 2){
+				fprintf(stderr, "Too few arguments\n");
+			}
+		return SYSERR;
+		}
 
 		return 0;
 }
