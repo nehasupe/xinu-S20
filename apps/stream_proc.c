54b4119975e9a6a2499c5a5b94d2038f39b2d7ae
nehasupe <nehasupe@github.iu.edu>
2020-03-05 19:56:25 +0000

    fix

--- apps/stream_proc.c
+++ apps/stream_proc.c
@@ -2,6 +2,7 @@
 #include <stream_proc.h>
 //#include "tscdf_input1.h"
 #include "tscdf.h"
+//#include "tscdf_input.h"
 uint pcport;
 int work_queue_depth;
 int num_streams;
@@ -11,6 +12,7 @@ int output_time;
 void stream_producer(struct stream** stream_index){
 	int i = 0;
 	int stream_id;
+	//printf("work queue depth: %d\n", work_queue_depth);
 	while(1){
 		struct data_element *d;
 	        d = (struct data_element*)getmem(sizeof(de));
@@ -20,10 +22,11 @@ void stream_producer(struct stream** stream_index){
 		d -> time = atoi(a);
 		while (*a++ != '\t');
 		d -> value = atoi(a);
+		//kprintf("stream id: %d, time: %d, value: %d\n", stream_id, d -> time, d -> value);
 		wait(stream_index[stream_id] -> spaces);
 		wait(stream_index[stream_id] -> mutex);
-		//printf("%d\n", d -> value);
 		(stream_index[stream_id] -> queue)[stream_index[stream_id] -> tail] = *d;
+		//kprintf("%d produced time: %d value: %d\n", stream_id, d->time, d->value);
 		stream_index[stream_id] -> tail = (stream_index[stream_id] -> tail +1) % work_queue_depth;
 		signal(stream_index[stream_id] -> mutex);
 		signal(stream_index[stream_id] -> items);
@@ -36,6 +39,8 @@ void stream_consumer(int32 id, struct stream *str){
 	int32 t;
 	char output[100];
 	int counter = 0;
+	//printf("time window: %d\n", time_window);
+	//printf("output time: %d\n", output_time);
 	struct tscdf* tc = tscdf_init(time_window);
 	kprintf("stream_consumer id:%d (pid:%d)\n", id, getpid()); 
 	while(1){
@@ -44,9 +49,9 @@ void stream_consumer(int32 id, struct stream *str){
 		c = (str -> queue)[str -> head].value;
 		t = (str -> queue)[str -> head].time;
 		str -> head = (str -> head + 1) % work_queue_depth;
-		//signal(str -> mutex);
-		//signal(str -> spaces);			
-		if(t==0){
+		//kprintf("%d consumed value: %d, time: %d",id, c, t);
+		
+		if(t==0 && c==0){
 			kprintf("stream_consumer exiting\n");
 			ptsend(pcport, getpid());
 			return;
@@ -65,10 +70,14 @@ void stream_consumer(int32 id, struct stream *str){
 	 	kprintf("%s\n", output);	       
 	 	freemem((char *)qarray, (6*sizeof(int32)));
 		}
+		//if(t==0 && c==0){
+		//	kprintf("stream_consumer exiting\n");
+		//	ptsend(pcport, getpid());
+		//	return;
+		//}
 		signal(str -> mutex);
 		signal(str -> spaces);
 
-	//ptsend(pcport, getpid());
 	}
 }
 
@@ -125,46 +134,18 @@ int stream_proc(int nargs, char* args[]) {
 		return(-1);
 		}
 
-  	// Your code for Phase 1 goes here
-	// Use `i` as the stream id.
-	// Create queues, producer creates the queues
-	// num_streams need to be created, use an array to store the pointer to each stream, array type will be struct stream
 	struct stream* stream_index[num_streams];
 	int i;
 	for (i = 0; i < num_streams; i++) {
 		stream_index[i] = (struct stream*) getmem(sizeof(struct stream));
-		//initialize the queue for each consumer processes
-		//size of the queue is work depth of the queue
-		//time window?
-		// set the variables
-		//stream_index[i] -> queue = getmem(work_queue_depth*sizeof(struct data_element));
-		//struct data_element *po;
-	        //po= (struct data_element*)getmem(work_queue_depth*sizeof(de));
-	        //po= (struct data_element*)getmem(work_queue_depth*sizeof(de));
-		//struct data_element (stream_index[i] -> queue)[work_queue_depth]; 
-		//stream_index[i] -> queue = po;
 		stream_index[i] -> queue = (struct data_element*)getmem(work_queue_depth*sizeof(struct data_element));
 		stream_index[i] -> head = 0;
 		stream_index[i] -> tail = 0;
 		stream_index[i] -> mutex = semcreate(1);
 		stream_index[i] -> spaces = semcreate(work_queue_depth);
 		stream_index[i] -> items = semcreate(0);
-		//printf("created %d streams", i);
-
+		
 	}
-	// Parse input header file data and populate work queue
-	 //Single producer reading input streams
-	 //// this has to go in so sort of loop and then put in queues.
-	//char* a = (char *)stream_input[i];
-	//int st = atoi(a);
-	//while (*a++ != '\t');
-	//int ts = atoi(a);
-	//while (*a++ != '\t');
-	//int v = atoi(a);
-	//enqueue the data
-	//(stream_index[1] -> queue)[0].time = 21;
-	//(stream_index[1] -> queue)[0].value = 42;
-	//Creating consumer processes
 	resume( create(stream_producer, 1024, 20, "stream_producer", 1, stream_index));
 	for(i = 0; i < num_streams; i++){
 		resume (create(stream_consumer, 1024, 20, "stream_consumer", 2, i, stream_index[i]));
@@ -173,7 +154,7 @@ int stream_proc(int nargs, char* args[]) {
 	for(i=0; i < num_streams; i++) {
 		uint32 pm;
 		pm = ptrecv(pcport);
-   		kprintf("process %d exited\n", pm);
+   	kprintf("process %d exited\n", pm);
                 }
    
         ptdelete(pcport, 0);
